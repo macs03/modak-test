52e1592f897ba870080eea60b6b023273972d888
Miguel Cardenas <miguelcarde.3@gmail.com>
2022-11-04 12:27:20 -0500

    feat: screens and navigation by folders

--- src/screens/ModalScreen/ModalScreen.js
+++ src/screens/ModalScreen/ModalScreen.js
@@ -1,8 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet } from "react-native";
+import { Platform } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import styles from "./styles";
 
 export default function ModalScreen() {
   return (
@@ -20,20 +21,3 @@ export default function ModalScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold"
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%"
-  }
-});
--- src/screens/TabOneScreen/TabOneScreen.js
+++ src/screens/TabOneScreen/TabOneScreen.js
@@ -1,7 +1,9 @@
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+
+import styles from "./styles";
 
 export default function TabOneScreen({ navigation }) {
   return (
@@ -16,20 +18,3 @@ export default function TabOneScreen({ navigation }) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold"
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%"
-  }
-});
