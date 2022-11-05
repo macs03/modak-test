b10992721d6fee2244d0899f54b8ab93c26ea2eb
Miguel Cardenas <miguelcarde.3@gmail.com>
2022-11-04 23:16:19 -0500

    feat: persisting data

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import React, { useReducer } from "react";
+import React, { useReducer, useState, useEffect } from "react";
 import {
   reduceFavorites,
-  initialState
+  initialState,
+  getStoredData
 } from "./src/lib/store/reducers/favorites";
 
 import FavoritesContext from "./src/lib/store/FavoritesContext";
+import { addFavorite } from "./src/lib/store/actions/actions";
 
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
 
 export default function App() {
+  const [initialStateData, setInitialStateData] = useState(initialState);
+  const [state, dispatch] = useReducer(reduceFavorites, initialStateData);
+
+  useEffect(() => {
+    getStoredData()
+      .then(data => {
+        data?.favorites.forEach(row => {
+          dispatch(addFavorite(row));
+        });
+      })
+      .catch(error => console.error(error));
+  }, []);
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  const [state, dispatch] = useReducer(reduceFavorites, initialState);
 
   if (!isLoadingComplete) {
     return null;
--- src/lib/store/reducers/favorites.js
+++ src/lib/store/reducers/favorites.js
@@ -1,6 +1,25 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { FavoritesTypes } from "../actions/actions";
 
 const { ADD_FAVORITE, REMOVE_FAVORITE, REMOVE_ALL_FAVORITES } = FavoritesTypes;
+
+async function saveData(dataToStored) {
+  try {
+    const jsonValue = JSON.stringify(dataToStored);
+    await AsyncStorage.setItem("@app_data", jsonValue);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const getStoredData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem("@app_data");
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.error(error);
+  }
+};
 
 export const initialState = { favorites: [] };
 
@@ -17,17 +36,29 @@ export function reduceFavorites(state, action) {
 
   switch (type) {
     case ADD_FAVORITE:
-      return {
+      const newState = {
         ...state,
         favorites: [...state.favorites, payload]
       };
+
+      saveData(newState);
+
+      return newState;
     case REMOVE_FAVORITE:
-      return {
+      const newStateAfterDelete = {
         ...state,
         favorites: deleteFavorites(state, payload)
       };
+
+      saveData(newStateAfterDelete);
+
+      return newStateAfterDelete;
     case REMOVE_ALL_FAVORITES:
-      return { favorites: [] };
+      const stateRestated = { favorites: [] };
+
+      saveData(stateRestated);
+
+      return stateRestated;
     default:
       return state;
   }
